68ddf7490e512acf94a18efc6ed97cade975175e
Csaba Szirjak <csszirjak@instructure.com>
2019-04-13 10:58:50 -0600

    try to commit and push

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -6,7 +6,7 @@ class Player {
     fun betRequest(game_state: JSONObject): Int {
         val allIn = Random.nextInt(0, 100)
 
-        return if(allIn < 10) { 1000 } else { 0 }
+        return if(allIn <= 10) { 1000 } else { 0 }
     }
 
     fun showdown() {
