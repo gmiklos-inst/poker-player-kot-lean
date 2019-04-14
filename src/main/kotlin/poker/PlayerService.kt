2dec790b6ae2c1babd0771584bd630d9bce3947f
Gabriel Miklos <gmiklos@instructure.com>
2019-04-13 23:51:27 -0600

    A bit more modernised version. Still need to find a good way to process JSON without blocking inside the coroutine.

--- src/main/kotlin/poker/PlayerService.kt
+++ src/main/kotlin/poker/PlayerService.kt
@@ -1,43 +1,51 @@
 package poker
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import org.wasabifx.wasabi.app.AppConfiguration
-import org.wasabifx.wasabi.app.AppServer
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.request.receiveParameters
+import io.ktor.response.respondText
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import poker.model.GameState
 
 fun main() {
-    val server = AppServer(AppConfiguration(getPort()))
-    val player = PlayerLogic()
+    embeddedServer(Netty, getPort()) {
+        val player = PlayerLogic()
 
-    server.post("/", {
-        val action = request.bodyParams["action"]
-        val result = when (action) {
-            "bet_request" -> {
-                val gameStateJson = request.bodyParams["game_state"] as? String
+        install(CallLogging)
 
-                if (gameStateJson == null) {
-                    "Missing game_state!"
-                } else {
-                    val gameState = jacksonObjectMapper().readValue(gameStateJson, GameState::class.java)
-                    player.betRequest(gameState).toString()
+        routing {
+            post("/") {
+                val parameters = call.receiveParameters()
+
+                val action = parameters["action"]
+                val result = when (action) {
+                    "bet_request" -> {
+                        val gameStateJson = parameters["game_state"]
+
+                        if (gameStateJson == null) {
+                            "Missing game_state!"
+                        } else {
+                            val gameState = jacksonObjectMapper().readValue(gameStateJson, GameState::class.java)
+                            player.betRequest(gameState).toString()
+                        }
+                    }
+                    "showdown" -> {
+                        player.showdown()
+                        "OK"
+                    }
+                    "version" -> player.version()
+                    else -> "Unknown action '$action'!"
                 }
+
+                call.respondText(result)
             }
-            "showdown" -> {
-                player.showdown()
-                "OK"
-            }
-            "version" -> player.version()
-            else -> "Unknown action '$action'!"
         }
-
-        response.send(result)
-    })
-
-    server.start()
+    }.start(wait = true)
 }
 
-private fun getPort(): Int {
-    val port = System.getenv("PORT") ?: "2345"
-
-    return Integer.parseInt(port)
-}
+private fun getPort() = System.getenv("PORT")?.let { Integer.parseInt(it) } ?: 2345
